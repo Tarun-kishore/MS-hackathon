76e5e13dc6729f549dca16dd2987b049f09148b3
anoushkagera <anoushkagera28@gmail.com>
2022-06-04 21:27:35 +0530

    some updates

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,4 @@
 
 // ! all database information are saved in environment
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/toybankDB")
