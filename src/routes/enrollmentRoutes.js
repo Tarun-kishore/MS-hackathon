f6a6e95869aa8d220bbf3af989a79f46a8197897
Ashish-verma-NSUT <vermaashish322@gmail.com>
2022-06-07 00:26:42 +0530

    fixes

--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { isVolunteer, isAdmin } = require("../middleware/userRoles");
 const router = express.Router();
 const Event = require("../models/event");
+const User = require("../models/user");
+
 const auth = require("../middleware/auth");
 //list of routes for enrollment
 
