68a350f67eca3245a5dea816863bc58257935b41
Tarun Kishore <tarun2kishore@gmail.com>
2022-06-08 13:02:01 +0530

    added alternate routes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const app = require("./app.js");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 // *defining port default at 3000
 const port = process.env.PORT || 3001;
 
