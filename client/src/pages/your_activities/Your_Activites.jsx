ebbdd37ebabb9196f0b161d04b4fae1ae94a66d5
manas-bohat <manasbohat01@gmail.com>
2022-06-10 01:36:22 +0530

    Updates

--- client/src/pages/your_activities/Your_Activites.jsx
+++ client/src/pages/your_activities/Your_Activites.jsx
@@ -6,11 +6,16 @@ import axios from "axios"
 export default function Your_Activites() {
 
   const [cards, setCards] = useState([]);
+  // no activity
+  const [nact, setNact] = useState(false);
 
   useEffect(() => {
     const fetchCards = async () => {
       const res = await axios.get("/event/recommended");
-      console.log(res);
+      console.log(res.data.length);
+      if(res.data.length === 0) {
+        setNact(true);
+      }
       setCards(res.data);
     }
     fetchCards()
@@ -20,8 +25,6 @@ export default function Your_Activites() {
     e.preventDefault();
     try {  
     const res = await axios.get("/volunteer/myEvents");
-
-    console.log(res);
     setCards(res.data);
     
   } catch(err) {
@@ -42,17 +45,26 @@ export default function Your_Activites() {
     }
   }
 
-  return (
-    <>
-    <div className='heading'>
-         <span className='waste'></span>
-         <span className='act'>Activities Coming Up</span>
-         <span className='all' onClick={handleAll}> ALL </span>
-         <span className='enrol' onClick={handleEnrolled}> ENROLLED </span>
+  if(!nact) {
+    return (
+      <>
+      <div className='heading'>
+          <span className='waste'></span>
+          <span className='act'>Activities Coming Up</span>
+          <span className='all' onClick={handleAll}> ALL </span>
+          <span className='enrol' onClick={handleEnrolled}> ENROLLED </span>
+        </div>
+      <div className='your_activities'>
+        <Cards cards={cards}/>
+        </div>
+      </>  
+    )
+  } else {
+    return (
+      <div className="message">
+        <h1 className='big'> No Activities to show</h1>
+        <h3 className='small'> Have you registered for volunteering activities yet? If no, please register asap!</h3>
       </div>
-    <div className='your_activities'>
-      <Cards cards={cards}/>
-      </div>
-    </>  
-  )
+    )
+  }
 }
